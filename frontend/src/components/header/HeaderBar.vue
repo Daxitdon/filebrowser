d7e30f305f6e7bbb401deefeb86b78c38c270bbf
daxit <daxitm2112@gmail.com>
2023-08-14 17:44:03 +0530

    removed action component as well

--- frontend/src/components/header/HeaderBar.vue
+++ frontend/src/components/header/HeaderBar.vue
@@ -1,28 +1,10 @@
 <template>
-  <header>
-    <img v-if="showLogo !== undefined" :src="logoURL" />
-    
-
-    <slot />
-
-    <div id="dropdown" :class="{ active: this.$store.state.show === 'more' }">
-      <slot name="actions" />
-    </div>
-
-    
-
-    <div
-      class="overlay"
-      v-show="this.$store.state.show == 'more'"
-      @click="$store.commit('closeHovers')"
-    />
-  </header>
+  
 </template>
 
 <script>
 import { logoURL } from "@/utils/constants";
 
-import Action from "@/components/header/Action";
 
 export default {
   name: "header-bar",
